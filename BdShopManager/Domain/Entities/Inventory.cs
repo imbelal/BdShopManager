ee0351d1951fed380a15551268d0f9e4cea07e29
imbelal <imbelalhosen@gmail.com>
2024-01-01 01:40:49 +0100

    Inventory entity updated with private set for properties.

--- BdShopManager/Domain/Entities/Inventory.cs
+++ BdShopManager/Domain/Entities/Inventory.cs
@@ -5,12 +5,12 @@ namespace Domain.Entities
 {
     public class Inventory : AuditableEntityBase, IAggregateRoot, ISoftDeletable
     {
-        public Guid ProductId { get; set; }
-        public Guid SupplierId { get; set; }
-        public int Quantity { get; set; }
-        public decimal CostPerUnit { get; set; }
-        public decimal TotalCost { get; set; }
-        public string Remark { get; set; }
+        public Guid ProductId { get; private set; }
+        public Guid SupplierId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal CostPerUnit { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public string Remark { get; private set; }
         public bool IsDeleted { get; set; } = false;
 
         public Inventory(Guid productId, Guid supplierId, int quantity, decimal costPerUnit, decimal totalCost, string remark)
